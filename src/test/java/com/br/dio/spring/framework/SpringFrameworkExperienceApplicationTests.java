739f1fd1dd7340baa201a4ad571132fab9096eb1
Mateus Rodrigues do Nascimento <90523108+mateusrdn@users.noreply.github.com>
2022-08-01 13:45:52 -0300

    commit inicial

---
+++ src/test/java/com/br/dio/spring/framework/SpringFrameworkExperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dio.spring.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkExperienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
